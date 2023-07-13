460a242ac487216e28810f6e9034111dc78fec9f
Christian <c.espinoza.code@gmail.com>
2023-07-13 17:33:54 -0500

    initial html structure created for baseLayout container

---
+++ src/components/Container/BaseLayout/index.jsx
@@ -0,0 +1,19 @@
+import './styles.css';
+import { useMachine } from '@xstate/react';
+import { BookingMachine } from '../../../machines/BookingMachine';
+import { Nav } from '../../Nav';
+import { StepsLayout } from '../StepsLayout';
+
+function BaseLayout() {
+  const [state, send] = useMachine(BookingMachine);
+
+  console.log(state.matches('search'));
+  return (
+    <div className="BaseLayout">
+      <Nav state={state} send={send} />
+      <StepsLayout state={state} send={send} />
+    </div>
+  );
+}
+
+export { BaseLayout };
