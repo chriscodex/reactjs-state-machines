576407fde81550615c1ac5668775a2f9dc782a63
Christian <c.espinoza.code@gmail.com>
2023-07-14 23:09:34 -0500

    logic for every state created in steps layout component

--- src/components/Container/StepsLayout/index.jsx
+++ src/components/Container/StepsLayout/index.jsx
@@ -2,10 +2,15 @@ import './styles.css';
 import { Welcome } from '../../Welcome';
 import { Search } from '../../Search';
 import { Tickets } from '../../Tickets';
+import { Passengers } from '../../Passengers';
 
 function StepsLayout({ state, send }) {
   const renderContent = () => {
-    return <Tickets state={state} send={send}/>;
+    if(state.matches('initial')) return <Welcome send={send}/>;
+    if(state.matches('search')) return <Search state={state} send={send}/>;
+    if(state.matches('tickets')) return <Tickets send={send} context={state.context}/>;
+    if(state.matches('passengers')) return <Passengers state={state} send={send}/>;
+    return null;
   };
 
   return <div className="StepsLayout">{renderContent()}</div>;
