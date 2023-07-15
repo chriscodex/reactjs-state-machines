7b203e170291cbfce8c619efe25d028c5c0280c0
Christian <c.espinoza.code@gmail.com>
2023-07-14 23:22:30 -0500

    thanks messsage changed

--- src/components/Tickets/index.jsx
+++ src/components/Tickets/index.jsx
@@ -7,7 +7,7 @@ function Tickets({ send, context }) {
 
   return (
     <div className='Tickets'>
-      <p className='Tickets-description description'>Gracias por volar con book a fly 💚</p>
+      <p className='Tickets-description description'>Gracias por volar con nosotros</p>
       <div className='Tickets-ticket'>
         <div className='Tickets-country'>Colombia</div>
         <div className='Tickets-passengers'>
