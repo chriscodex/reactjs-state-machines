e14597137f618318f950057cc8363543db903333
Christian <c.espinoza.code@gmail.com>
2023-07-14 23:11:20 -0500

    nav title changed to flight tickets

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -7,7 +7,7 @@ export const Nav = ({ state, send }) => {
 
   return (
     <nav className='Nav'>
-      <h1 className='Nav-logo'>Book a ticket 🎫</h1>
+      <h1 className='Nav-logo'>Flight tickets ✈</h1>
       {!state.matches('initial') && !state.matches('tickets') &&
         <button onClick={goToWelcome} className='Nav-cancel button-secondary'>Cancelar</button>
       }
